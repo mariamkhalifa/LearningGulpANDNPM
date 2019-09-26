227a269f558ea23ae9450133319002fa29c842d7
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-09-26 13:07:09 -0400

    pushed repo

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const nano = require('cssnano');
+const prefixer = require('autoprefixer');
+const imagemin = require('gulp-imagemin');
+
+// define some common tasks for gulp to run
+
+// like compile and minify SASS files
+function compile(done) {
+    gulp.src('./sass/**/*.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss([prefixer(),nano()]))
+        .pipe(gulp.dest('./css'))
+        done()
+}
+
+//minify every image
+function squashImages() {
+    gulp.src('./images/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'))
+        done()
+}
+
+exports.compile = compile;
+exports.squash = squashImages;
